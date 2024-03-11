37a73172227129cf0f6cc6766b724c53f7dde0c2
PrasannaAmale11 <amaleprasanna2001@gmail.com>
2024-03-11 15:17:25 +0530

    Deployment settings

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 
 import express from 'express';
 import dotenv from 'dotenv'
-
+import path from 'path'
 import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
 import userRoutes from './routes/user.routes.js';
@@ -11,7 +11,9 @@ import cookieParser from 'cookie-parser'
 import { app, server } from './socket/socket.js';
 
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -23,10 +25,12 @@ app.use("/api/auth" , authRoutes);
 app.use("/api/messages" , messageRoutes);
 app.use("/api/users" , userRoutes);
 
+app.use(express.static(path.join(__dirname,"/client/dist")));
 
-// app.get("/", (req, res) => {
-//   res.send("Welcome howllw")
-// })
+app.get("*",(req, res) => {
+  res.sendFile(path.join(__dirname,"client", "dist","index.html"))
+})
+
 
 
 
